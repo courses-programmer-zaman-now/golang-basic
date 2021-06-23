63f3ed6cc338c8be8d25105111a9e387e60e262b
danil <danilsyaharihardjo@gmail.com>
2021-06-23 09:02:16 +0700

    update interface

--- 27_interface/main.go
+++ 27_interface/main.go
@@ -4,35 +4,56 @@ import "fmt"
 
 type HasName interface {
 	GetName() string
+	GetAge() int
 }
 
 func SayHello(hasname HasName) {
 	fmt.Println("Hello", hasname.GetName())
 }
 
-type Person struct{
+func StatusAge(hasname HasName) {
+	if hasname.GetAge() >= 17 {
+		fmt.Println("Dewasa")
+	} else {
+		fmt.Println("Kecil")
+	}
+}
+
+type Person struct {
 	Name string
+	Age  int
 }
 
 func (person Person) GetName() string {
 	return person.Name
 }
 
-type Animal struct{
+func (person Person) GetAge() int {
+	return person.Age
+}
+
+type Animal struct {
 	Name string
+	Age  int
 }
 
 func (animal Animal) GetName() string {
 	return animal.Name
 }
 
+func (animal Animal) GetAge() int {
+	return animal.Age
+}
+
 func main() {
 	var danil Person
 	danil.Name = "Danil Syah"
+	danil.Age = 27
 	SayHello(danil)
+	StatusAge(danil)
 
-	dog := Animal {
-		Name : "fitbull",
+	dog := Animal{
+		Name: "fitbull",
 	}
 	SayHello(dog)
 }
